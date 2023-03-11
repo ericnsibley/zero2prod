3ff2566d8e9fb59b0f8f0959ba1c8bd642b3d544
ericnsibley <ericnsibley@gmail.com>
2023-03-11 13:47:27 -0500

    added db, configuration, and reorganized repo

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -64,7 +64,7 @@ async fn subscribe_returns_400_when_data_is_missing() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address.");
     // Concurrent, not necessarily parallel
     let _ = tokio::spawn(server);
 
