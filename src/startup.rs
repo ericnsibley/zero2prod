205bee121494866bf1dcbc0ef584661bfbe287dd
ericnsibley <ericnsibley@gmail.com>
2023-04-01 16:40:27 -0400

    ch3

--- src/startup.rs
+++ src/startup.rs
@@ -3,15 +3,19 @@ use routes::{greet, health_check, subscribe};
 use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // PgPool isn't Clone so it needs an Arc
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(greet))
             .route("/name/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
